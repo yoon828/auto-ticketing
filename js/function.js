5983c47ee100812fda267b6fc6e1b2ec3d919432
yoon828 <yoon828990@gmail.com>
2024-09-05 20:17:27 +0900

    feat: 예매창 띄우기

--- js/function.js
+++ js/function.js
@@ -1,146 +1,173 @@
 const puppeteer = require('puppeteer');
 const log = require('./js/log.js');
 
-async function startTicketing(consertId, numberPerson, day, userId, pw) {
+//ticket lint tickecting
+async function startTicketing(numberPerson, userId, pw) {
     const browser = await puppeteer.launch({
-        headless: false,
-        args: ['--disable-web-security', '--disable-features=IsolateOrigins', ' --disable-site-isolation-trials']
+      headless: false,
+      args: [
+        "--disable-web-security",
+        "--disable-features=IsolateOrigins",
+        " --disable-site-isolation-trials",
+      ],
     });
-
+  
     let page = await browser.newPage();
-    await page.goto('https://ticket.interpark.com/Gate/TPLogin.asp');
-    
-    await page.setViewport({width: 1080, height: 1024});
-
-    await page.waitForSelector('iframe');
+    await page.goto("https://ticket.interpark.com/Gate/TPLogin.asp");
+  
+    await page.setViewport({ width: 1080, height: 1024 });
+  
+    await page.waitForSelector("iframe");
     const id = await page.$(
-        'iframe[src="https://accounts.interpark.com/authorize/ticket-pc?origin=https%3A%2F%2Fticket%2Einterpark%2Ecom%2FGate%2FTPLoginConfirmGate%2Easp%3FGroupCode%3D%26Tiki%3D%26Point%3D%26PlayDate%3D%26PlaySeq%3D%26HeartYN%3D%26TikiAutoPop%3D%26BookingBizCode%3D%26MemBizCD%3DWEBBR%26CPage%3D%26GPage%3Dhttp%253A%252F%252Fticket%252Einterpark%252Ecom%252F&postProc=IFRAME"]',
+      'iframe[src="https://accounts.interpark.com/authorize/ticket-pc?origin=https%3A%2F%2Fticket%2Einterpark%2Ecom%2FGate%2FTPLoginConfirmGate%2Easp%3FGroupCode%3D%26Tiki%3D%26Point%3D%26PlayDate%3D%26PlaySeq%3D%26HeartYN%3D%26TikiAutoPop%3D%26BookingBizCode%3D%26MemBizCD%3DWEBBR%26CPage%3D%26GPage%3Dhttp%253A%252F%252Fticket%252Einterpark%252Ecom%252F&postProc=IFRAME"]'
     );
-
+  
     // 로그인
-    const idSelector = '#userId';
-    const idInput = userId;
-    const pwSelector = '#userPwd';
-    const pwInput = pw;
-
+    const idSelector = "#userId";
+    const idInput = "k915k1004";
+    const pwSelector = "#userPwd";
+    const pwInput = "godqhrgo09!";
+  
     let frame = await id.contentFrame();
     await frame.type(idSelector, idInput);
     await frame.type(pwSelector, pwInput);
-    page.keyboard.press('Enter');
+    page.keyboard.press("Enter");
     await log.addLog("로그인 성공");
-    
-    await page.waitForSelector('#DivMainPopup');
-
+  
+    // await page.waitForSelector("#DivMainPopup");
+    await page.waitForSelector("#contents");
+  
     // save cookie
     const cookies = await page.cookies();
-
+  
     // load cookie
     await page.setCookie(...cookies);
-
+  
     // consert
-    let consertUrl = 'https://tickets.interpark.com/goods/' + consertId;
-    await page.goto(consertUrl);
-    
+    let gameUrl =
+      "https://ticket.interpark.com/Contents/Sports/GoodsInfo?SportsCode=07001&TeamCode=PB003";
+    await page.goto(gameUrl);
+  
     // if exsited popup -> close
-    const popupCloseBut = '#popup-prdGuide > div > div.popupFooter > button'
+    const popupCloseBut =
+      "#div_checkDontsee_PT002_4_1 > div.popupInput > button.btn.btnClose";
+  
     const closeButElement = await page.$(popupCloseBut);
-    if(closeButElement != null) {
-        await page.click(popupCloseBut);
+    if (closeButElement != null) {
+      await page.click(popupCloseBut);
     }
-
-    // 예매 오픈 시간까지 기다리기
-    // let startTime = new Date(2023, 3, 28, 11, 59, 59);
-    // let now = new Date();
-    // await new Promise(r => setTimeout(r, startTime.getTime() - now.getTime()));
-    // page.reload();
-
-    const ticketSelector = '.stickyWrap';
-    await page.waitForSelector(ticketSelector);
+  
+    /**
+     * 
+    //   예매 오픈 시간까지 기다리기
+    let startTime = new Date(2024, 8, 3, 11, 29, 59);
+    let now = new Date();
+    const diff = startTime.getTime() - now.getTime();
+    await log.addLog("시작 대기 : " + diff / 1000.0 + "seconds");
+    await new Promise((r) => setTimeout(r, diff));
+    page.reload();
     
-    // 날짜
-    let mutedDayLength = (await page.$$('.muted')).length;
-    let dayCalculate = mutedDayLength + Number(day);
+    await log.addLog("시간 완료!");
     
-    const daySelector = '#productSide > div > div.sideMain > div.sideContainer.containerTop.sideToggleWrap > div.sideContent.toggleCalendar > div > div > div > div > ul:nth-child(3) > li:nth-child(' + `${dayCalculate}` + ')';
-    await page.waitForSelector(daySelector);
-    page.click(daySelector);
-    await log.addLog("공연 관람일 클릭");
-
+    */
+  
+    // const ticketSelector = ".stickyWrap";
+    // await page.waitForSelector(ticketSelector);
+  
+    //   // 날짜
+    //   let mutedDayLength = (await page.$$(".muted")).length;
+    //   let dayCalculate = mutedDayLength + Number(day);
+  
+    //   const daySelector =
+    //     "#productSide > div > div.sideMain > div.sideContainer.containerTop.sideToggleWrap > div.sideContent.toggleCalendar > div > div > div > div > ul:nth-child(3) > li:nth-child(" +
+    //     `${dayCalculate}` +
+    //     ")";
+    //   await page.waitForSelector(daySelector);
+    //   page.click(daySelector);
+    //   await log.addLog("공연 관람일 클릭");
+  
     // 예매하기
     // TODO : 티켓 오픈 예정 시간 입력 받아서 처리하는 로직
     // let startTime = new Date(2023, 3, 28, 11, 59, 59);
     // let now = new Date();
     // await new Promise(r => setTimeout(r, startTime.getTime() - now.getTime()));
     // await log.addLog("end wait");
-
-    const buttonSelector = '#productSide > div > div.sideBtnWrap > a.sideBtn.is-primary';
+  
+    const buttonSelector =
+      "div.timeScheduleList > div.timeSchedule.lineColor_Y > div.btns   > a.BtnColor_Y.btn1";
     page.click(buttonSelector);
-    const newPagePromise = await new Promise(x => page.once('popup', x));
+    const newPagePromise = await new Promise((x) => page.once("popup", x));
     await log.addLog("예매하기 버튼 클릭 성공");
-    await page.setViewport({width: 1080, height: 1024});
+    await page.setViewport({ width: 1080, height: 1024 });
     page = await newPagePromise;
-
+  
+    return;
+  
     // 새창
-    await page.waitForSelector('#divBookSeat');
-    let iframeWindow = await page.$(
-        'iframe[id="ifrmSeat"]'
-    );
-    
+    await page.waitForSelector("#divBookSeat");
+    let iframeWindow = await page.$('iframe[id="ifrmSeat"]');
+  
     frame = await iframeWindow.contentFrame();
-
+  
     // 잠깐 접어두기 클릭
     await Promise.all([
-        await frame.waitForSelector('#divCaptchaFolding > a'),
-        await frame.click('#divCaptchaFolding > a'),
+      await frame.waitForSelector("#divCaptchaFolding > a"),
+      await frame.click("#divCaptchaFolding > a"),
     ]);
-
+  
     // 좌석 선택
-    await frame.waitForSelector('#ifrmSeatDetail');
-    iframeWindow = await frame.$(
-        'iframe[id="ifrmSeatDetail"]'
-    );
+    await frame.waitForSelector("#ifrmSeatDetail");
+    iframeWindow = await frame.$('iframe[id="ifrmSeatDetail"]');
     let datailFrame = await iframeWindow.contentFrame();
-    
+  
     // 직접 구역 누르고
     // GetBlockSeatList('', '', '016');
     // #TmgsTable > tbody > tr > td > map > area:nth-child(16)
-
+  
     // #divSeatBox 나오면 자동으로 좌석 클릭
-    await datailFrame.waitForSelector('#divSeatBox');
+    await datailFrame.waitForSelector("#divSeatBox");
     const seatArr = await datailFrame.$$('span[class="SeatN"]');
-    
+  
     for (let index = 0; index < numberPerson; index++) {
-        if(index+1 > seatArr.length) {
-            log.addErrorLog("잔여좌석 " + seatArr.length + "개로 " + numberPerson-seatArr.length + "개의 좌석은 잡지 못했습니다.");
-            break;
-        }
-        await seatArr[index].click();
-        await log.addLog("select seat");
+      if (index + 1 > seatArr.length) {
+        log.addErrorLog(
+          "잔여좌석 " +
+            seatArr.length +
+            "개로 " +
+            numberPerson -
+            seatArr.length +
+            "개의 좌석은 잡지 못했습니다."
+        );
+        break;
+      }
+      await seatArr[index].click();
+      await log.addLog("select seat");
     }
-
+  
     // 좌석 선택 완료 버튼 클릭
-    await frame.click('body > form:nth-child(2) > div > div.contWrap > div.seatR > div > div.btnWrap > a');
+    await frame.click(
+      "body > form:nth-child(2) > div > div.contWrap > div.seatR > div > div.btnWrap > a"
+    );
     await log.addLog("좌석 선택 완료");
-
+  
     sleep(50000);
-
+  
     // 사용자가 수기로 문자열 입력
-
-    
+  
     // await Promise.all([
     //     frame.click(arr[0]),
     //     frame.click(arr[1])
     // ]);
-    
+  
     // frame.click(area의 title이 036영역);
-
+  
     // 완료
-
+  
     // 문자열 입력 -> 결제 알아서 완료하기
     // const imgId = '#imgCaptcha';
     // await frame.waitForSelector(imgId);
     // let imgUrl = await frame.$eval(imgId, el => el.getAttribute('src'));
-
+  
     // const worker = await createWorker();
     // (async () => {
     //     await worker.loadLanguage('eng');
@@ -150,8 +177,7 @@ async function startTicketing(consertId, numberPerson, day, userId, pw) {
     //     await worker.terminate();
     //   })();
     return true;
-}
-
+  }
 function sleep(ms) {
     return new Promise((r) => setTimeout(r, ms));
 }
